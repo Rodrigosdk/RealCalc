2b75a47e2db1b52dd039c1aee8340018b2feb273
rodrigosdk <rodrigomamede53@gmail.com>
2026-09-04 23:53:37 -0300

    feat: Refatorar o AppModule para estender o CoreModule e atualizar o AppModuleTest com a integração do SelicCubit

--- lib/modules/app_module.dart
+++ lib/modules/app_module.dart
@@ -1,17 +1,15 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:real_calc/core/routes/app_routes.dart';
+import 'package:real_calc/core/module.dart';
 
 import 'financing/module.dart';
 import 'future_value/module.dart';
 import 'home/module.dart';
 import 'regular_deposits/module.dart';
 
-class AppModule extends Module {
+class AppModule extends CoreModule {
   @override
   void binds(Injector i) {}
-
-  @override
-  void exportedBinds(Injector i) {}
 
   @override
   void routes(RouteManager r) {
