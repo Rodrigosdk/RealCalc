8873ef0d2b315c6efe9d9f982ed1fb522e26c069
Rodrigo Mamede <61851910+Rodrigosdk@users.noreply.github.com>
2026-09-14 15:18:18 -0300

    fix: Corrigir instância ausente de RegularDepositsCubit no AppModuleTest (#50)

--- test/test_app_module.dart
+++ test/test_app_module.dart
@@ -3,6 +3,7 @@ import 'package:real_calc/modules/app_module.dart';
 import 'package:real_calc/modules/financial_calculators/presentation/cubit/financing/financing_cubit.dart';
 import 'package:real_calc/modules/financial_calculators/presentation/cubit/forms/financing_form_cubit.dart';
 import 'package:real_calc/modules/financial_calculators/presentation/cubit/future_value/future_value_cubit.dart';
+import 'package:real_calc/modules/financial_calculators/presentation/cubit/regular_deposits/regular_deposits_cubit.dart';
 import 'package:real_calc/modules/home/presentation/cubit/greeting_cubit.dart';
 import 'package:real_calc/modules/home/presentation/cubit/selic_cubit.dart';
 
@@ -12,6 +13,7 @@ class AppModuleTest extends AppModule {
   final SelicCubit selicCubit;
   final GreetingCubit greetingCubit;
   final FinancingFormCubit financingFormCubit;
+  final RegularDepositsCubit regularDepositsCubit;
 
   AppModuleTest({
     required this.financingCubit,
@@ -19,15 +21,16 @@ class AppModuleTest extends AppModule {
     required this.selicCubit,
     required this.greetingCubit,
     required this.financingFormCubit,
+    required this.regularDepositsCubit,
   });
 
   @override
   void binds(Injector i) {
-    super.binds(i);
     i.addInstance<FinancingCubit>(financingCubit);
     i.addInstance<FutureValueCubit>(futureValueCubit);
     i.addInstance<SelicCubit>(selicCubit);
     i.addInstance<GreetingCubit>(greetingCubit);
     i.addInstance<FinancingFormCubit>(financingFormCubit);
+    i.addInstance<RegularDepositsCubit>(regularDepositsCubit);
   }
 }
