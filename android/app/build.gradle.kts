cec7cea30897569482949a9b6e583cb2b89934ba
rodrigosdk <rodrigomamede53@gmail.com>
2026-09-22 14:04:21 -0300

    feat: atualizar a configuração do Android Gradle e melhorar a configuração de assinatura

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,44 +1,40 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
-def keystoreProperties = new Properties()
-def keystorePropertiesFile = rootProject.file('key.properties')
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
 if (keystorePropertiesFile.exists()) {
-    keystoreProperties.load(new FileInputStream(keystorePropertiesFile))
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 
 android {
     namespace = "br.com.rodrigosdk.realcalc"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
+    
     signingConfigs {
-        release {
+        create("release") {
             if (keystorePropertiesFile.exists()) {
-                storeFile = file(keystoreProperties['storeFile'])
-                storePassword = keystoreProperties['storePassword']
-                keyAlias = keystoreProperties['keyAlias']
-                keyPassword = keystoreProperties['keyPassword']
+                storeFile = file(keystoreProperties.getProperty("storeFile"))
+                storePassword = keystoreProperties.getProperty("storePassword")
+                keyAlias = keystoreProperties.getProperty("keyAlias")
+                keyPassword = keystoreProperties.getProperty("keyPassword")
             }
         }
     }
+    
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
-
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "br.com.rodrigosdk.realcalc"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -47,12 +43,19 @@ android {
 
     buildTypes {
         release {
-            // Associa a assinatura configurada acima ao build de release
-            signingConfig signingConfigs.release
+            // Associa a assinatura configurada no formato Kotlin DSL
+            signingConfig = signingConfigs.getByName("release")
             
-            minifyEnabled true
-            shrinkResources true
+            // Ativa o R8/ProGuard usando atribuição direta
+            isMinifyEnabled = true
+            isShrinkResources = true        
         }
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
     }
 }
 
